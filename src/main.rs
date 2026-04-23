6c2414c389609fb69ac31b32199c5ea28c590429
x1unix <9203548+x1unix@users.noreply.github.com>
2026-04-22 21:38:42 -0400

    handle PING
    Signed-off-by: x1unix <9203548+x1unix@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 #![allow(unused_imports)]
+use anyhow::Result;
 use std::net::TcpListener;
+
+pub mod proto;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        if let Err(err) = handle_incoming(stream) {
+            println!("error: {}", err);
         }
     }
+}
+
+fn handle_incoming(r: Result<std::net::TcpStream, std::io::Error>) -> Result<()> {
+    let mut s = r?;
+    proto::read_stream(&mut s)?;
+    Ok(())
 }
---
+++ src/proto.rs
@@ -0,0 +1,21 @@
+use std::net::TcpStream;
+
+use anyhow::{Context, Error, Result, anyhow};
+
+pub fn read_stream<T>(s: &mut T) -> Result<()>
+where
+    T: std::io::Read + std::io::Write,
+{
+    // TODO: read all
+    let mut buff: [u8; 8] = [0; 8];
+    let n = s.read(&mut buff[..])?;
+    let req = str::from_utf8(&buff[..n]).context("payload is not a text")?;
+    let response = match req {
+        "PING" => "+PONG\r\n",
+        _ => return Err(anyhow!("invalid cmd: {}", req)),
+    };
+
+    s.write_all(response.as_bytes())
+        .context("can't write response")
+}
+
